0ed0b8a0fdc19766aa8e143dab09c6f1072b030e
Slime <lolwutrofl333@gmail.com>
2019-09-15 21:51:27 -0700

    finished up the RPS app on windows

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let aiScore = 0;
 const humanScoreSpan = document.getElementById("human-score");
 const aiScoreSpan = document.getElementById("ai-score");
 const scoreBoardDiv = document.querySelector(".scores");
-const resultDiv = document.queryCommandValue(".results");
+const resultDiv = document.querySelector(".results > p");
 const paperDiv = document.getElementById("paper");
 const rockDiv = document.getElementById("rock");
 const scissorsDiv = document.getElementById("scissors");
@@ -11,51 +11,69 @@ const scissorsDiv = document.getElementById("scissors");
 
 
 
-
 function getComputerPlay() {
     const choices = ["paper", "rock", "scissors"];
-    const randomChoices = choices[Math.floor(Math.random()*3)];
-    return randomChoices;
+    const randomChoices = [Math.floor(Math.random()*3)];
+    return choices[randomChoices];
 }
+getComputerPlay();
 
-function winner() {
-    humanScore++;
+function main() {
+    paperDiv.addEventListener('click', function() {
+        game("paper");
+    });
+    rockDiv.addEventListener('click', function() {
+        game("rock");
+    });
+    scissorsDiv.addEventListener('click', function() {
+        game("scissors");
+    })
 }
-
-function losses() {
-    aiScore++;
-}
+main();
 
 function game(humanChoice) {
     const aiChoice = getComputerPlay();
     switch (humanChoice + aiChoice) {
-        case "rock, scissors":
-        case "paper, rock":
-        case "scissors, paper":
-            console.log("Human Wins!");
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            winner(humanChoice, aiChoice);
             break;
-        case "scissors. rock":
-        case "paper, scissors":
-        case "rock, paper":
-            console.log("AI Wins!");
+        case "scissorsrock":
+        case "paperscissors":
+        case "rockpaper":
+            losses(humanChoice, aiChoice);
             break;
-        case "rock, rock":
-        case "paper, paper":
-        case "scissors, scissors":
-            console.log("Draw!");
+        case "rockrock":
+        case "paperpaper":
+        case "scissorsscissors":
+            draw(humanChoice, aiChoice);
             break;
     }
 
 }
+game();
+let humanChoice = game(); 
+let aiChoice = getComputerPlay();
 
-paperDiv.addEventListener('click', function() {
-    game("paper");
-});
+function winner(human, ai) {
+    humanScore++;
+    humanScoreSpan.innerHTML = humanScore;
+    aiScoreSpan.innerHTML = aiScore;
+    resultDiv.innerHTML = humanChoice + " destroys " + aiChoice + " You win! Run it back.";
+}
 
-rockDiv.addEventListener('click', function() {
-    game("rock");
-});
+function losses(human, ai) {
+    aiScore++;
+    aiScoreSpan.innerHTML = aiScore;
+    humanScoreSpan.innerHTML = humanScore;
+    resultDiv.innerHTML = humanChoice + " gets destroyed by " + aiChoice + " You lose! Try again.";
+}
 
-scissorsDiv.addEventListener('click', function() {
-    game("scissors");
-});
+function draw(human, ai) {
+    resultDiv.innerHTML = humanChoice + " you both can do better " + aiChoice + " It was a draw! Try again.";
+
+}
+
+
+
